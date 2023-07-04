680dcf8d0913c36920cae1a2a8ecefe3023995bb
antonio celano <antonio_celano@hotmail.com>
2023-07-04 15:10:03 +0200

    Improve responsiveness

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import App from "./components/App";
 // BrowserRouter helps with seamless transitions while switching between components.
 // Basically, it will only reload/refresh the component that needs to be changed instead of refreshing/reloading the entire page.
 // Though BrowserRouter is not a necessity, it is a must if you want your app to be responsive.
+// GitHub Pages does not support browser history like your browser does.
+// To overcome this problem, we need to use a Hash Router instead of a Browser Router in our application.
+// This type of router uses the hash portion of the URL to keep the UI in sync with the URL.
 import { HashRouter as Router } from "react-router-dom";
 
 // Your custom CSS file. You can use a single file and import it either on your index.js or your App.js file,
