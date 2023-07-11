95fe2dd90e7db41729eecbd5db5f39c5c969b8c2
antonio celano <antonio_celano@hotmail.com>
2023-07-11 16:49:13 +0200

    Increase responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,32 +10,32 @@ import {
 function Navbar() {
   return (
     <div>
-        <nav className="navbar navbar-expand-lg">
-          <div className="container-fluid">
-            <button className="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+          <button className="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-              <ul className="navbar-nav ms-auto mt-2 mt-lg-0">
-                <li className="nav-item ms-auto">
-                  <Link to="/"><button className="btn btn-light btn-lg navbar-button m-2">Home</button></Link>
-                </li>
-                <li className="nav-item ms-auto">
-                  <Link to="/about"><button className="btn btn-light btn-lg navbar-button m-2">About</button></Link>
-                </li>
-              </ul>
-            </div>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ms-auto mt-2 mt-lg-0">
+              <li className="nav-item ms-auto">
+                <Link to="/"><button className="btn btn-lg navbar-button m-2 button">Home</button></Link>
+              </li>
+              <li className="nav-item ms-auto">
+                <Link to="/about"><button className="btn btn-lg navbar-button m-2 button">About</button></Link>
+              </li>
+            </ul>
           </div>
-        </nav>
-        <Routes>
-          <Route path="/about" element={<>
-          </>} />
-          <Route path="/" element={<>
-            <div className="container-fluid">
-            </div>
-          </>} />
-        </Routes>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/about" element={<>
+        </>} />
+        <Route path="/" element={<>
+          <div className="container-fluid">
+          </div>
+        </>} />
+      </Routes>
     </div>
   );
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,48 +6,48 @@ export default function Projects() {
     <div id="projects">
       <div className="container container-sub">
         <div class="row">
-          <div class="col-12 col-md-6 my-5">
+          <div class="col-12 col-md-12 col-lg-6 my-5">
             <h3>List of Employees</h3>
             <p>A website to list employees, created with the MERN stack. Solely responsible for the website's creation.</p>
             <ControlledCarousel />
             <a href="https://github.com/acelanoit/MERN-App---List-of-Employees" target="_blank" rel="noreferrer">
-              <button className="btn button-small">GitHub</button>
+              <button className="btn btn-sm button m-2">GitHub</button>
             </a>
             <a href="https://mern-list-of-employees.vercel.app/" target="_blank" rel="noreferrer">
-              <button className="btn button-small">Deployment</button>
+              <button className="btn btn-sm button m-2">Deployment</button>
             </a>
           </div>
-          <div class="col-12 col-md-6 my-5">
+          <div class="col-12 col-md-12 col-lg-6 my-5">
             <h3>Case Converter</h3>
             <p>A case converter created with React. Solely responsible for the website's creation.</p>
             <ControlledCarousel />
             <a href="https://github.com/acelanoit/case-converter" target="_blank" rel="noreferrer">
-              <button className="btn button-small">GitHub</button>
+              <button className="btn btn-sm button m-2">GitHub</button>
             </a>
             <a href="https://acelanoit.github.io/case-converter/" target="_blank" rel="noreferrer">
-              <button className="btn button-small">Deployment</button>
+              <button className="btn btn-sm button m-2">Deployment</button>
             </a>
           </div>
-          <div class="col-12 col-md-6 my-5">
+          <div class="col-12 col-md-12 col-lg-6 my-5">
             <h3>Hobber</h3>
             <p>A mobile website created with Ruby on Rails for gamers to find gaming partners. Responsible for 80% of the back end.</p>
             <ControlledCarousel />
             <a href="https://github.com/eaudepaul/Hobber" target="_blank" rel="noreferrer">
-              <button className="btn button-small">GitHub</button>
+              <button className="btn btn-sm button m-2">GitHub</button>
             </a>
             <a href="https://www.hobber.eu/" target="_blank" rel="noreferrer">
-              <button className="btn button-small">Deployment</button>
+              <button className="btn btn-sm button m-2">Deployment</button>
             </a>
           </div>
-          <div class="col-12 col-md-6 my-5">
+          <div class="col-12 col-md-12 col-lg-6 my-5">
             <h3>Halloween Dog</h3>
             <p>A side-scrolling video game created with HTML canvas and JavaScript. Followed an online tutorial for the base game and then added more features to it myself.</p>
             <ControlledCarousel />
             <a href="https://github.com/acelanoit/halloween-dog" target="_blank" rel="noreferrer">
-              <button className="btn button-small">GitHub</button>
+              <button className="btn btn-sm button m-2">GitHub</button>
             </a>
             <a href="https://acelanoit.github.io/halloween-dog/" target="_blank" rel="noreferrer">
-              <button className="btn button-small">Deployment</button>
+              <button className="btn btn-sm button m-2">Deployment</button>
             </a>
           </div>
         </div>
