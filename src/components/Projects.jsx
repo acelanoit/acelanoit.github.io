09e56b4609ab0119200412d54bcc27c52e1a88e6
antonio celano <antonio_celano@hotmail.com>
2023-07-13 17:33:41 +0200

    Make project descriptions roughly same length for increased responsiveness

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,7 +9,7 @@ export default function Projects() {
         <div className="row">
           <div className="col-12 col-md-12 col-lg-6 my-5">
             <h3>List of Employees</h3>
-            <p>A website to list employees, created with the MERN stack. Solely responsible for the website's creation.</p>
+            <p>A website to list employees, created with the MERN stack. I was solely responsible for developing the entire website from scratch. The website allows to perform all CRUD operations, providing a user-friendly interface and seamless functionality.</p>
             <ControlledCarousel img1="images/1-1.png" img2="images/1-2.png" img3="images/1-3.png" blank="images/blank-horizontal.png" />
             <a href="https://github.com/acelanoit/MERN-App---List-of-Employees" target="_blank" rel="noreferrer">
               <button className="btn btn-sm button m-2">GitHub</button>
@@ -20,7 +20,7 @@ export default function Projects() {
           </div>
           <div className="col-12 col-md-12 col-lg-6 my-5">
             <h3>Halloween Dog</h3>
-            <p>A side-scrolling video game created using HTML canvas and JavaScript. Enhanced a tutorial-based game with additional features.</p>
+            <p>A side-scrolling video game created with HTML canvas and JavaScript. I took an existing tutorial-based game and expanded its features, enhancing the experience by adding new elements and challenges that made the game more engaging.</p>
             <ControlledCarousel img1="images/2-1.png" img2="images/2-2.png" img3="images/2-3.png" blank="images/blank-horizontal.png" />
             <a href="https://github.com/acelanoit/halloween-dog" target="_blank" rel="noreferrer">
               <button className="btn btn-sm button m-2">GitHub</button>
@@ -31,7 +31,7 @@ export default function Projects() {
           </div>
           <div className="col-12 col-md-12 col-lg-6 my-5">
             <h3>Hobber</h3>
-            <p>A mobile website created with Ruby on Rails for gamers to find gaming partners. Responsible for 80% of the back end.</p>
+            <p>A mobile website designed specifically for gamers to connect and find gaming partners, created with Ruby on Rails. I played a crucial role in developing the backend of the website, contributing to approximately 80% of its functionality.</p>
             <ControlledCarousel img1="images/3-1.png" img2="images/3-2.png" img3="images/3-3.png" blank="images/blank-horizontal.png" />
             <a href="https://github.com/eaudepaul/Hobber" target="_blank" rel="noreferrer">
               <button className="btn btn-sm button m-2">GitHub</button>
@@ -42,7 +42,7 @@ export default function Projects() {
           </div>
           <div className="col-12 col-md-12 col-lg-6 my-5">
             <h3>Case Converter</h3>
-            <p>A case converter created with React. Solely responsible for the website's creation.</p>
+            <p>A React-based case converter application. I took full ownership of the project, creating a website that allows users to convert text between different letter cases. The user-friendly interface facilitates easy text input and conversion.</p>
             <ControlledCarousel img1="images/4-1.png" img2="images/4-2.png" img3="images/4-3.png" blank="images/blank-horizontal.png" />
             <a href="https://github.com/acelanoit/case-converter" target="_blank" rel="noreferrer">
               <button className="btn btn-sm button m-2">GitHub</button>
