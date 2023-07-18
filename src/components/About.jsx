a15fd7780e3a004f7229434c2e8e6be63475095d
antonio celano <antonio_celano@hotmail.com>
2023-07-18 16:26:04 +0200

    Move about text to right of profile picture

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,9 +4,19 @@ export default function About() {
   return (
     <div id="about">
       <div className="container container-sub p-2 pt-5 pb-5">
-        <img className="profile-pic rounded-circle" src="images/profile.jpg" alt="profile" />
-        <h2 className="mt-4">Hello, World!</h2>
-        <p className="mx-2 mb-0">As an aspiring coding professional, I have pursued my passion for programming through various avenues, including completing an online coding course and recently graduating from the prestigious Le Wagon coding bootcamp. While I appreciate the aesthetics and user experience considerations of front-end development, my true passion lies in what remains invisible to the user and in building the logic of applications.</p>
+
+        <div className="row">
+          <div className="col-12 col-md-6 container-sub about-box">
+            <img className="profile-pic rounded-circle" src="images/profile.jpg" alt="profile" />
+          </div>
+          <div className="col-12 col-md-6 container-sub about-box">
+            <div>
+              <div className="mb-3 d-none d-sm-block d-md-none"></div>
+              <h2>Hello, World!</h2>
+              <p className="mx-2 mb-0">As an aspiring coding professional, I have pursued my passion for programming through various avenues, including completing an online coding course and recently graduating from the prestigious Le Wagon coding bootcamp. While I appreciate the aesthetics and user experience considerations of front-end development, my true passion lies in what remains invisible to the user and in building the logic of applications.</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
