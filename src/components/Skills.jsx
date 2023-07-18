1f2637df458adcf62b2d72a1b8a6ddbb2ab065d1
antonio celano <antonio_celano@hotmail.com>
2023-07-18 14:07:05 +0200

    Add skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,16 +4,62 @@ export default function Skills() {
   return (
     <div id="about">
       <div className="container container-sub">
-        <h2 className="mt-4">The languages I speak</h2>
-        <div>
-          <img className="devicon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" alt="html" />
-          <img className="devicon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" alt="css" />
-          <img className="devicon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original-wordmark.svg" alt="bootstrap" />
-          <img className="devicon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="javascript" />
-          <img className="devicon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" />
-          <img className="devicon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original-wordmark.svg" alt="express" />
-          <img className="devicon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg" alt="mongodb" />
-          <img className="devicon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" alt="react" />
+        <h2 className="my-4">The languages I speak:</h2>
+        <div className="row">
+          <div className="col-6 col-md-4 col-lg-2 my-3">
+            <img className="devicon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original-wordmark.svg" alt="bootstrap" />
+          </div>
+          <div className="col-6 col-md-4 col-lg-2 my-3">
+            <img className="devicon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" alt="css" />
+          </div>
+          <div className="col-6 col-md-4 col-lg-2 my-3">
+            <img className="devicon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original-wordmark.svg" alt="express" />
+          </div>
+          <div className="col-6 col-md-4 col-lg-2 my-3">
+            <img className="devicon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" alt="figma" />
+          </div>
+          <div className="col-6 col-md-4 col-lg-2 my-3">
+            <img className="devicon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original-wordmark.svg" alt="git" />
+          </div>
+          <div className="col-6 col-md-4 col-lg-2 my-3">
+            <img className="devicon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt="github" />
+          </div>
+          <div className="col-6 col-md-4 col-lg-2 my-3">
+            <img className="devicon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-original-wordmark.svg" alt="heroku" />
+          </div>
+          <div className="col-6 col-md-4 col-lg-2 my-3">
+            <img className="devicon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" alt="html" />
+          </div>
+          <div className="col-6 col-md-4 col-lg-2 my-3">
+            <img className="devicon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="javascript" />
+          </div>
+          <div className="col-6 col-md-4 col-lg-2 my-3">
+            <img className="devicon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg" alt="mongodb" />
+          </div>
+          <div className="col-6 col-md-4 col-lg-2 my-3">
+            <img className="devicon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" />
+          </div>
+          <div className="col-6 col-md-4 col-lg-2 my-3">
+            <img className="devicon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg" alt="postgresql" />
+          </div>
+          <div className="col-6 col-md-4 col-lg-2 my-3">
+            <img className="devicon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rails/rails-original-wordmark.svg" alt="rails" />
+          </div>
+          <div className="col-6 col-md-4 col-lg-2 my-3">
+            <img className="devicon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" alt="react" />
+          </div>
+          <div className="col-6 col-md-4 col-lg-2 my-3">
+            <img className="devicon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-original-wordmark.svg" alt="ruby" />
+          </div>
+          <div className="col-6 col-md-4 col-lg-2 my-3">
+            <img className="devicon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlite/sqlite-original-wordmark.svg" alt="sqlite" />
+          </div>
+          <div className="col-6 col-md-4 col-lg-2 my-3">
+            <img className="devicon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" alt="typescript" />
+          </div>
+          <div className="col-6 col-md-4 col-lg-2 my-3">
+            <img className="devicon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original-wordmark.svg" alt="vuejs" />
+          </div>
         </div>
       </div>
     </div>
