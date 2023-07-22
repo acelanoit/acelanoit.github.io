fc5cd98bd38cbda09e37a0d2d91135c9fda7b7df
antonio celano <antonio_celano@hotmail.com>
2023-07-22 21:24:03 +0200

    Move CSS folder inside public folder

--- src/index.js
+++ src/index.js
@@ -2,12 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 
-// Your custom CSS file. You can use a single file and import it either on your index.js or your App.js file,
-// or use multiple files (like App.css and so on) and import them only where needed.
-// Styles defined in the CSS file imported will override styles defined in different CSS files imported in parent components
-// if they refer to the same element(s):
-import "./styles/index.css";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
