746ad6a9c20ada65c536c25eecccb97f9b29423c
antonio celano <antonio_celano@hotmail.com>
2024-01-03 10:58:31 +0100

    Add material ui skill

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,67 +9,70 @@ export default function Skills() {
         <h2 className="mb-5">The languages I speak:</h2>
         <div className="row">
           <div className="col-6 col-md-4 col-lg-2 my-3">
-            <img className={isMobile ? "devicon w-25" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-original.svg" alt="angular" />
+            <img className={isMobile ? "devicon w-50" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-original.svg" alt="angular" />
           </div>
           <div className="col-6 col-md-4 col-lg-2 my-3">
-            <img className={isMobile ? "devicon w-25" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original-wordmark.svg" alt="bootstrap" />
+            <img className={isMobile ? "devicon w-50" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original-wordmark.svg" alt="bootstrap" />
           </div>
           <div className="col-6 col-md-4 col-lg-2 my-3">
-            <img className={isMobile ? "devicon w-25" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" alt="css" />
+            <img className={isMobile ? "devicon w-50" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" alt="css" />
           </div>
           <div className="col-6 col-md-4 col-lg-2 my-3">
-            <img className={isMobile ? "devicon w-25" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original-wordmark.svg" alt="express" />
+            <img className={isMobile ? "devicon w-50" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original-wordmark.svg" alt="express" />
           </div>
           <div className="col-6 col-md-4 col-lg-2 my-3">
-            <img className={isMobile ? "devicon w-25" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" alt="figma" />
+            <img className={isMobile ? "devicon w-50" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" alt="figma" />
           </div>
           <div className="col-6 col-md-4 col-lg-2 my-3">
-            <img className={isMobile ? "devicon w-25" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original-wordmark.svg" alt="git" />
+            <img className={isMobile ? "devicon w-50" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original-wordmark.svg" alt="git" />
           </div>
           <div className="col-6 col-md-4 col-lg-2 my-3">
-            <img className={isMobile ? "devicon w-25" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt="github" />
+            <img className={isMobile ? "devicon w-50" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt="github" />
           </div>
           <div className="col-6 col-md-4 col-lg-2 my-3">
-            <img className={isMobile ? "devicon w-25" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-original-wordmark.svg" alt="heroku" />
+            <img className={isMobile ? "devicon w-50" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-original-wordmark.svg" alt="heroku" />
           </div>
           <div className="col-6 col-md-4 col-lg-2 my-3">
-            <img className={isMobile ? "devicon w-25" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" alt="html" />
+            <img className={isMobile ? "devicon w-50" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" alt="html" />
           </div>
           <div className="col-6 col-md-4 col-lg-2 my-3">
-            <img className={isMobile ? "devicon w-25" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="javascript" />
+            <img className={isMobile ? "devicon w-50" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="javascript" />
           </div>
           <div className="col-6 col-md-4 col-lg-2 my-3">
-            <img className={isMobile ? "devicon w-25" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg" alt="mongodb" />
+            <img className={isMobile ? "devicon w-50" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg" alt="materialui" />
           </div>
           <div className="col-6 col-md-4 col-lg-2 my-3">
-            <img className={isMobile ? "devicon w-25" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg" alt="nextjs" />
+            <img className={isMobile ? "devicon w-50" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg" alt="mongodb" />
           </div>
           <div className="col-6 col-md-4 col-lg-2 my-3">
-            <img className={isMobile ? "devicon w-25" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" />
+            <img className={isMobile ? "devicon w-50" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg" alt="nextjs" />
           </div>
           <div className="col-6 col-md-4 col-lg-2 my-3">
-            <img className={isMobile ? "devicon w-25" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg" alt="postgresql" />
+            <img className={isMobile ? "devicon w-50" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" />
           </div>
           <div className="col-6 col-md-4 col-lg-2 my-3">
-            <img className={isMobile ? "devicon w-25" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rails/rails-original-wordmark.svg" alt="rails" />
+            <img className={isMobile ? "devicon w-50" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg" alt="postgresql" />
           </div>
           <div className="col-6 col-md-4 col-lg-2 my-3">
-            <img className={isMobile ? "devicon w-25" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" alt="react" />
+            <img className={isMobile ? "devicon w-50" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rails/rails-original-wordmark.svg" alt="rails" />
           </div>
           <div className="col-6 col-md-4 col-lg-2 my-3">
-            <img className={isMobile ? "devicon w-25" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-original-wordmark.svg" alt="ruby" />
+            <img className={isMobile ? "devicon w-50" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" alt="react" />
           </div>
           <div className="col-6 col-md-4 col-lg-2 my-3">
-            <img className={isMobile ? "devicon w-25" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlite/sqlite-original-wordmark.svg" alt="sqlite" />
+            <img className={isMobile ? "devicon w-50" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-original-wordmark.svg" alt="ruby" />
           </div>
           <div className="col-6 col-md-4 col-lg-2 my-3">
-            <img className={isMobile ? "devicon w-25" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg" alt="tailwindcss" />
+            <img className={isMobile ? "devicon w-50" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlite/sqlite-original-wordmark.svg" alt="sqlite" />
           </div>
           <div className="col-6 col-md-4 col-lg-2 my-3">
-            <img className={isMobile ? "devicon w-25" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" alt="typescript" />
+            <img className={isMobile ? "devicon w-50" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg" alt="tailwindcss" />
           </div>
           <div className="col-6 col-md-4 col-lg-2 my-3">
-            <img className={isMobile ? "devicon w-25" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original-wordmark.svg" alt="vuejs" />
+            <img className={isMobile ? "devicon w-50" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" alt="typescript" />
+          </div>
+          <div className="col-6 col-md-4 col-lg-2 my-3">
+            <img className={isMobile ? "devicon w-50" : "devicon"} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original-wordmark.svg" alt="vuejs" />
           </div>
         </div>
       </div>
